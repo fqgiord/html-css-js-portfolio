982f70695631149070f3a2a95300312e187b7021
Fernando Giordano <fqgiord@gmail.com>
2024-04-04 10:43:56 -0300

    transition's effect
    included transitions into principal info's portfolio

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const elements = document.querySelectorAll(".about-details-container, .experience-details-container, .contact");
+
+elements.forEach((element) => myObserver.observe(element));
